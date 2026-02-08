1a6c42d8caeda29c1b6bb9fbb2750af7aaba8c0f
unknown <1040606497@qq.com>
2026-02-08 17:37:29 +0800

    更新一下最近的内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   "title": "Haruka的技术小窝",
   "description": "新主题的博客",
   "dest": "public",
-  // "base":"/new-blog/",  //base路径，你的github项目的名称，github页面需要添加此行
+  "base":"/new-blog/",  //base路径，你的github项目的名称，github页面需要添加此行
   "head": [
     [
       "link",
